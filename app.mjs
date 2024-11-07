bd16ecaee8476aa234a023abf6905fae68655ebf
Sirawit-Boon <ter.sirawit1997@gmail.com>
2024-11-07 14:25:03 +0700

    crate server from express

---
+++ app.mjs
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/profiles", (req, res) => {
+  return res.json({
+    data: {
+      name: "john",
+      age: 20,
+    },
+  });
+});
+
+app.get("/", (req,res) => {
+    return res.send(`Hello express`)
+})
+
+app.listen(port, () => {
+    console.log(`Server is running at ${port}`);
+  });
+
